f9e1791902c9a9a917458d8641f84be07c6ab087
michellebinder <binder_michelle@web.de>
2022-01-30 17:25:17 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddbookComponent } from './addbook/addbook.component';
+import { LatestbookComponent } from './latestbook/latestbook.component';
+import { OldestbookComponent } from './oldestbook/oldestbook.component';
+import { ViewbooksComponent } from './viewbooks/viewbooks.component';
+
+const routes: Routes = [ { path:'', component:ViewbooksComponent}, { path:'addbook', component:AddbookComponent}, { path:'latestbook', component:LatestbookComponent}, { path:'oldestbook', component:OldestbookComponent} ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
